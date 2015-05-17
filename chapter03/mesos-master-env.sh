916bf0a4d5a036de13198d7dab4dcc70bb4516f8
Roger Ignazio <me@rogerignazio.com>
2015-05-17 15:21:36 -0700

    example mesos master and slave config files
    This commit introduces example Mesos master and slave configuration
    files, for when Mesos has been compiled from source. These files should
    be placed at `/usr/local/mesos/etc/mesos/$file`, assuming that Mesos was
    installed to `/usr/local/mesos`.
    
    For a complete list of configuration options for Mesos masters and
    slaves, see the output of `mesos-{master,slave} --help`.

---
+++ chapter03/mesos-master-env.sh
@@ -0,0 +1,4 @@
+export MESOS_zk=zk://mesos-master-1.manning.com:2181,mesos-master-2.manning.com:2181,mesos-master-3.manning.com/mesos
+export MESOS_quorum=2
+export MESOS_work_dir=/var/lib/mesos
+export MESOS_log_dir=/var/log/mesos
---
+++ chapter03/mesos-slave-env.sh
@@ -0,0 +1,3 @@
+export MESOS_zk=zk://mesos-master-1.manning.com:2181,mesos-master-2.manning.com:2181,mesos-master-3.manning.com/mesos
+export MESOS_work_dir=/var/lib/mesos
+export MESOS_log_dir=/var/log/mesos
