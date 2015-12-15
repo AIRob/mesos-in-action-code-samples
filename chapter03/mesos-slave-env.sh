8fba1554682ecfe0eb3084cf52ba2e234d00f93c
Roger Ignazio <me@rogerignazio.com>
2015-12-14 20:44:58 -0800

    (fixup) zk option should actually be master

--- chapter03/mesos-slave-env.sh
+++ chapter03/mesos-slave-env.sh
@@ -1,3 +1,3 @@
-export MESOS_zk=zk://mesos-master-1.example.com:2181,mesos-master-2.example.com:2181,mesos-master-3.example.com:2181/mesos
+export MESOS_master=zk://mesos-master-1.example.com:2181,mesos-master-2.example.com:2181,mesos-master-3.example.com:2181/mesos
 export MESOS_work_dir=/var/lib/mesos
 export MESOS_log_dir=/var/log/mesos
