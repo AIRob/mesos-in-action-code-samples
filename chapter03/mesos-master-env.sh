dc6a7966b4a2a8f86c47fc208ad4b25a65387c99
Roger Ignazio <me@rogerignazio.com>
2015-12-14 20:42:39 -0800

    (fixup) add missing zk port to mesos-master-3

--- chapter03/mesos-master-env.sh
+++ chapter03/mesos-master-env.sh
@@ -1,4 +1,4 @@
-export MESOS_zk=zk://mesos-master-1.example.com:2181,mesos-master-2.example.com:2181,mesos-master-3.example.com/mesos
+export MESOS_zk=zk://mesos-master-1.example.com:2181,mesos-master-2.example.com:2181,mesos-master-3.example.com:2181/mesos
 export MESOS_quorum=2
 export MESOS_work_dir=/var/lib/mesos
 export MESOS_log_dir=/var/log/mesos
